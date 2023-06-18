cd5145fca395bae436721dcf4dc730093a66d5b9
expresoviter <lesivvladyslav@gmail.com>
2023-06-18 19:00:27 +0300

    An interval and a symbol were chosen. Other small improvements.

--- tasks.py
+++ tasks.py
@@ -2,17 +2,14 @@ import requests
 from celery import Celery
 import csv
 
-app=Celery("tasks", broker='redis://localhost/0', backend='db+postgresql://postgres:marzipan@localhost/crypto')
+app = Celery("tasks", broker='redis://localhost/0', backend='db+postgresql://postgres:marzipan@localhost/crypto')
 @app.on_after_configure.connect
 def schedule_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(20.0, dataCollection.s("BTCUSDT","1h"))
+    sender.add_periodic_task(60.0, dataCollection.s())
 
 @app.task
-def dataCollection(symbol, interval):
-    if interval != "1d" and interval != "4h" and interval != "1h":
-        print("Entered interval is incorrect.")
-    else:
-        url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}"
+def dataCollection():
+        url = f"https://api.binance.com/api/v3/uiKlines?symbol=BTCUSDT&interval=1h"
 
         payload = {}
         headers = {}
@@ -27,14 +24,13 @@ def dataCollection(symbol, interval):
             with open("crypto.csv","a", newline="") as f:
                 write=csv.writer(f)
                 write.writerows(responseText)
+        print("The data for BTCUSDT and 1h interval:")
         print(responseText)
         return responseText
 
 
-if __name__=="__main__":
-    symbol = (input("Enter the symbol: ")).upper()
-    interval = input("Enter the interval (1d, 4h, 1h): ")
-    dataCollection(symbol,interval)
+if __name__ == "__main__":
+    dataCollection()
 
 #celery -A tasks worker -l info -P eventlet
 #celery -A tasks beat --loglevel=INFO
